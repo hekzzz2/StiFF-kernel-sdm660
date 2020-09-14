487719c1db20e796f930a5dee6d5e48dd0284ea0
Constantine Sapuntzakis <costa@purestorage.com>
2020-09-14 10:10:14 -0600

    ext4: fix superblock checksum calculation race
    commit acaa532687cdc3a03757defafece9c27aa667546 upstream.
    
    The race condition could cause the persisted superblock checksum
    to not match the contents of the superblock, causing the
    superblock to be considered corrupt.
    
    An example of the race follows.  A first thread is interrupted in the
    middle of a checksum calculation. Then, another thread changes the
    superblock, calculates a new checksum, and sets it. Then, the first
    thread resumes and sets the checksum based on the older superblock.
    
    To fix, serialize the superblock checksum calculation using the buffer
    header lock. While a spinlock is sufficient, the buffer header is
    already there and there is precedent for locking it (e.g. in
    ext4_commit_super).
    
    Tested the patch by booting up a kernel with the patch, creating
    a filesystem and some files (including some orphans), and then
    unmounting and remounting the file system.
    
    Cc: stable@kernel.org
    Signed-off-by: Constantine Sapuntzakis <costa@purestorage.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Suggested-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20200914161014.22275-1-costa@purestorage.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -144,7 +144,18 @@ void ext4_superblock_csum_set(struct super_block *sb)
 	if (!ext4_has_metadata_csum(sb))
 		return;
 
+	/*
+	 * Locking the superblock prevents the scenario
+	 * where:
+	 *  1) a first thread pauses during checksum calculation.
+	 *  2) a second thread updates the superblock, recalculates
+	 *     the checksum, and updates s_checksum
+	 *  3) the first thread resumes and finishes its checksum calculation
+	 *     and updates s_checksum with a potentially stale or torn value.
+	 */
+	lock_buffer(EXT4_SB(sb)->s_sbh);
 	es->s_checksum = ext4_superblock_csum(sb, es);
+	unlock_buffer(EXT4_SB(sb)->s_sbh);
 }
 
 void *ext4_kvmalloc(size_t size, gfp_t flags)
